5497f56a2d790351f85bb01948388f781881eade
M0jo570 <boejank699@gmail.com>
2025-11-29 20:59:20 +0700

    try to add /api/ in app.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${PORT}`));
